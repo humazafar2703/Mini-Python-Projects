fa460a6c6a0d850e6ef06b370e3425c8051fb757
Huma Zafar <87033635+humazafar2703@users.noreply.github.com>
2023-04-05 19:49:56 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,81 @@
+print ("Hello, World!")
+
+# multiline statement
+
+print ("Hello gals, \n today, weather is very cold")
+
+# multiline statement
+
+
+print (""""Hello gals, 
+
+today, weather is very cold""""")
+
+
+#python console acts as a calculator also does print type work
+'''2 * (10 + 3)
+5 - 6
+8 * 9
+6 / 2
+5 / 0
+5.0 / 2
+5 % 2
+2 * (10 + 3)
+2 ** 4
+abs(-11)  will work for integer
+abs(abs) will not work for string
+'''
+
+'''strings
+'abcdef1234' is the string as anything under quotation mark is called string'''
+
+'''print ("hello" + "hi")'''
+'''try these on console 
+"hello" *100
+"hello" + "hi" cocentate two strings
+'''
+
+'''print("hello"*100)'''
+''' console 
+int('one')
+string operator
+"Cat" * 3
+'CatCatCat'
+"Cat".upper()
+'CAT'
+"Cat".lower()
+'cat'
+"Cat".title()
+'Cat'
+dir(1) 
+tells what function you cam do on the data type example 1 
+typecasting int to str
+ type(int(str(100)))
+type('cat')
+print("cat" + 3 )
+print("cat" + '3' )
+print("cat" + str(3))
+
+'''
+
+print('spagetti')
+food = 'spagetti'
+print(food)
+
+oranges = 12
+
+cost_per_orange = 0.5
+
+total_cost =  oranges * cost_per_orange
+
+'''
+print(total_cost)
+print(int(total_cost))'''
+
+#print( int(oranges) + 'oranges') this wont print because the print function cant print different data type together
+
+
+print( str(oranges) + 'oranges')
+
+
+print( str(total_cost) + 'oranges')
